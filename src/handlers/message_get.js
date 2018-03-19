77120cfcf40ecae798a2dd8915100c35240e05b6
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-03-19 13:29:59 -0300

    get_message functionality

--- src/handlers/message_get.js
+++ src/handlers/message_get.js
@@ -45,27 +45,30 @@ class MessageGetHandler {
       let msg;
       messageId = event.pathParameters.id;
       try {
-        msg = await this.messageMgr.read(recipientId, messageId);
+        msg = await this.messageMgr.getMessage(recipientId, messageId);
         if (!msg) {
           cb({ code: 404, message: "message not found" });
+        }
+        if (msg.recipientId != recipientId) {
+          cb({ code: 403, message: "access to message forbidden" });
         }
         cb(null, { messages: msg });
         return;
       } catch (error) {
-        console.log("Error on this.messageMgr.read");
+        console.log("Error on this.messageMgr.getMessage");
         console.log(error);
         cb({ code: 500, message: error.message });
         return;
       }
     } else {
       try {
-        let messages = await this.messageMgr.read(recipientId);
+        let messages = await this.messageMgr.getAllMessages(recipientId);
         if (messages.length == 0) {
           cb({ code: 404, message: "message not found" });
         }
         cb(null, { messages: messages });
       } catch (error) {
-        console.log("Error on this.messageMgr.read");
+        console.log("Error on this.messageMgr.getAllMessages");
         console.log(error);
         cb({ code: 500, message: error.message });
         return;
