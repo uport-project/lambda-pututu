a0afc2aedf1f7828f167dcc812ea544ff5f23d7a
Cristóbal Castillo <1154738+c-castillo@users.noreply.github.com>
2018-06-06 17:17:54 -0400

    Fix CORS, other 403 errors (#3)
    * allow cors
    
    * some fixes
    
    * log auth header
    
    * use did-jwt to verify auth headers on sns
    
    * add audience to verifyJWT
    
    * fallback to previous circleci setting

--- src/lib/uPortMgr.js
+++ src/lib/uPortMgr.js
@@ -1,12 +1,17 @@
-require('ethr-did-resolver')()
-require('uport-did-resolver')()
-import { verifyJWT } from "did-jwt/lib/JWT";
+require("ethr-did-resolver")();
+require("uport-did-resolver")();
+import { verifyJWT, createJWT, decodeJWT } from "did-jwt/lib/JWT";
 
 class UportMgr {
-
   async verifyToken(token) {
     if (!token) throw "no token";
-    return verifyJWT(token);
+    const audience = decodeJWT(token).payload.aud;
+    return verifyJWT(token, { audience });
+  }
+
+  async createToken(options, payload) {
+    if (!payload) throw "no payload";
+    return createJWT(payload, options);
   }
 }
 module.exports = UportMgr;
