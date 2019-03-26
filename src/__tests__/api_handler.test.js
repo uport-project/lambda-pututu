40bd37b9b3c5a3c709b0359704008f07a2fcef5c
Andres Junge <ajunge@yaykuy.cl>
2019-03-26 13:56:27 -0300

    upgrade to new way

--- src/__tests__/api_handler.test.js
+++ src/__tests__/api_handler.test.js
@@ -1,39 +1,41 @@
-import AWS from "aws-sdk";
-import MockAWS from "aws-sdk-mock";
+const AWS = require("aws-sdk");
+const MockAWS = require ("aws-sdk-mock");
 MockAWS.setSDKInstance(AWS);
 
-const apiHandler = require("../api_handler");
+const apiHandler = require('../api_handler');
 
-describe("apiHandler", () => {
-  beforeAll(() => {
-    MockAWS.mock("KMS", "decrypt", Promise.resolve({ Plaintext: "{}" }));
-    process.env.SECRETS = "badSecret";
-  });
+describe('apiHandler', () => {
 
-  test("sns()", done => {
-    apiHandler.sns({}, {}, (err, res) => {
-      expect(err).toBeNull();
-      expect(res).not.toBeNull();
+    beforeAll(() => {
+        MockAWS.mock("KMS", "decrypt", Promise.resolve({Plaintext: "{}"}));
+        process.env.SECRETS="fakesecrets";
+    })
 
-      done();
+    test('sns()', done => {
+        apiHandler.sns({},{},(err,res)=>{
+            expect(err).toBeNull()
+            expect(res).not.toBeNull()
+            
+            done();
+        })
     });
-  });
 
-  test("message_get()", done => {
-    apiHandler.message_get({}, {}, (err, res) => {
-      expect(err).toBeNull();
-      expect(res).not.toBeNull();
-
-      done();
+    test('message_get()', done => {
+        apiHandler.message_get({},{},(err,res)=>{
+            expect(err).toBeNull()
+            expect(res).not.toBeNull()
+            
+            done();
+        })
     });
-  });
 
-  test("message_delete()", done => {
-    apiHandler.message_delete({}, {}, (err, res) => {
-      expect(err).toBeNull();
-      expect(res).not.toBeNull();
-
-      done();
+    test('message_delete()', done => {
+        apiHandler.message_delete({},{},(err,res)=>{
+            expect(err).toBeNull()
+            expect(res).not.toBeNull()
+            
+            done();
+        })
     });
-  });
+
 });
